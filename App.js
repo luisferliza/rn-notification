d8c7d0e0fd4d877f3955a6593e1859eb3a95cad3
llizama <llizama@launion.com.gt>
2023-06-14 09:30:36 -0600

    RNNotification

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import messaging from '@react-native-firebase/messaging'
+
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission()
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL
+
+  const token = await messaging().getToken()
+  console.log(token)
+
+  if (enabled) {
+    console.log('Authorization status:', authStatus)
+  }
+}
 
 export default function App() {
+  React.useEffect(() => {
+    requestUserPermission()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +35,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
